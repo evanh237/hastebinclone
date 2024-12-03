d609b6565ecc941f9914f2b2e068c137684ec85b
evanh237 <evanh237@gmail.com>
2024-12-03 11:15:40 -0600

    created and styled front end

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   const code = `Welcome to Wastebin!
-    
-    Use the commands in the top right corner
-    to create a new file to share with others! `;
+
+Use the commands in the top right corner
+to create a new file to share with others! `;
   res.render("code-display", { code, lineNumbers: code.split("\n").length });
 });
 
